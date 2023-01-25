096471e367c96a3468e5cb2d83440c12f1072a8c
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-25 20:11:37 +0300

    Сделал задание №41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введи числа через пробел: ");
+int[] myArray = GetArrayFromString(ReadLine());
+WriteLine($"В массиве {count(myArray)} чисел больше 0!");
+
+
+
+
+int[] GetArrayFromString(string arrayString)
+{
+    string[] nums = arrayString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = Convert.ToInt32(nums[i]);
+    }
+    return result;
+}
+
+
+
+int count(int[] Array)
+{
+    int result = 0;
+    foreach (var item in Array)
+    {
+        if (item > 0) result++;
+    }
+    return result;
+}
